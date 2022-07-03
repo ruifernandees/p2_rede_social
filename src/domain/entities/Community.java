1ec407c052c4eaabeaa77c680e1e14b25b80e36f
ruifernandees <dev.ruifernandes@gmail.com>
2022-07-03 10:35:01 -0300

    REFACTOR: remove Community show method

--- src/domain/entities/Community.java
+++ src/domain/entities/Community.java
@@ -20,16 +20,4 @@ public class Community {
     public void addUser(String user) {
         this.users.add(user);
     }
-
-    public void show() {
-        System.out.println("\n=== " + this.name + " ===");
-        System.out.println("Descrição: " + this.description);
-        System.out.println("Dono: " + this.ownerUsername);
-        System.out.println("Membros: ");
-        for (int i = 0; i < this.users.size(); i++) {
-            System.out.println(this.users.get(i));
-        }
-        System.out.println("\n=== Conteúdo ===");
-        this.content.show();
-    }
 }
